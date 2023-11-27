a001806dea1ca7a45e15924dc5ba9981d41c9c2b
amartinhua3 <amartinhua3@educacion.navarra.es>
2023-11-27 20:19:36 +0100

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,17 @@
+lista = ['Nombre Apellido 1', 'Nombre Apellido 2']
+
+def ListaNombres(Nombre_Apellido1, Nombre_Apellido2):
+    '''Esta función hace que devuelva la lista por orden alfabético de los apellidos
+    Parámetros
+        Nombre_Apellido1: Nombre de una persona
+        Nombre_Apellido2: Nombre de una persona
+    
+    Salida:
+        Devuelve la lista en orden alfabético de los apellidos'''
+    
+print(ListaNombres('A B', 'A A'))
+
+def OrdenarNombre(Nombre_Apellido):
+
+    print(lista.index('Apellido 1'))
+    return 
